47a355276e81a836cec9b103b15ef9668db4f379
Santripas <81268287+santripas@users.noreply.github.com>
2023-11-06 19:34:55 -0300

    desafio-gerando-biomas

---
+++ desafio-jornada-heroi/desafioteste.js
@@ -0,0 +1,14 @@
+const quantidadeGolpes = 3;
+
+// TODO: Defina aqui os tipos de minerais Carvao, Ferro, Diamante e Pedra
+let minerais = ["Carvão", "Ferro", "Diamante", "Pedra "];
+
+// Loop para cada golpe, de 1 até a quantidade informada
+for (let i = 1; i <= quantidadeGolpes; i++) { 
+    let minaIndex = (i-1) % minerais.length;
+    console.log( i + ': ' + minerais[minaIndex] );
+     }
+
+  
+  // TODO: Agora exiba o índice i, concatene com o caractere ":", após, concatene com tipo de minerais[minaIndex]:
+  console.log
