f196af725fe3bd6a2a45349a20b8e5e5f2e8fad4
Santripas <81268287+santripas@users.noreply.github.com>
2023-11-07 17:47:13 -0300

    estudos protótipo, funções construtoras

---
+++ aulas.js/aulas3.js
@@ -0,0 +1,50 @@
+//======== Protótipo, Funções Construtoras ==============
+/*
+const pessoa = {
+    genero:'masculino'
+}
+
+const sandro = {
+    nome: 'Sandro',
+    idade: 31,
+    __proto__:pessoa
+}
+
+
+console.log(sandro.genero);
+*/
+
+//========================================================
+/*
+function Pessoa(nome,idade) {
+    this.nome = nome
+    this.idade = idade
+}
+
+const sandro = new Pessoa('sandro',31)
+console.log(sandro)
+
+
+const pessoa = {
+    genero: 'masculino'
+}
+
+const sandro = Object.create(pessoa)
+
+sandro.nome = 'Sandro'
+
+console.log(sandro)
+*/
+
+//==================================================
+
+function Pessoa(nome, idade) {
+    this.nome = nome
+    this.idade = idade
+}
+
+Pessoa.prototype.falar = function () {
+    console.log(`Meu nome é: ${this.nome}`)
+}
+const sandro = new Pessoa('sandro',30)
+sandro.falar()
